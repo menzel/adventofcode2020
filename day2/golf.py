6dc30fa04199b659d3e2a7abcb1bdaa8221cb0a2
Michael Menzel <michael.menzel@mni.thm.de>
2020-12-09 13:06:29 +0100

    golfed day2

--- day2/golf.py
+++ day2/golf.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 with open("input") as fp:
-    print(sum([1 for line in fp if (line.split(": ")[1][int(line.split(": ")[0].split()[0].split("-")[0])-1] == line.split(": ")[0].split()[1]) ^ (line.split(": ")[1][int(line.split(": ")[0].split()[0].split("-")[1])-1] == line.split(": ")[0].split()[1])]))
+    print(sum([1 for l in fp if (l.split(": ")[1][int(l.split(": ")[0].split()[0].split("-")[0])-1] == l.split(": ")[0].split()[1]) ^ (l.split(": ")[1][int(l.split(": ")[0].split()[0].split("-")[1])-1] == l.split(": ")[0].split()[1])]))
